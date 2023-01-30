0b5eb6f7da44905946085579211d25e8e4aa3fa6
Nicholas Wong <sword9200@gmail.com>
2023-01-30 14:54:32 +0800

    style: custom theme

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -5,23 +5,34 @@
  */
 
 // Styles
-import '@mdi/font/css/materialdesignicons.css'
-import 'vuetify/styles'
+import "@mdi/font/css/materialdesignicons.css";
+import "vuetify/styles";
 
 // Composables
-import { createVuetify } from 'vuetify'
+import { createVuetify } from "vuetify";
+
+const myDefaultTheme = {
+  dark: true,
+  colors: {
+    background: "#2B2B2B",
+    surface: "#FFFFFF",
+    primary: "#6200EE",
+    "primary-darken-1": "#3700B3",
+    secondary: "#03DAC6",
+    "secondary-darken-1": "#018786",
+    error: "#B00020",
+    info: "#2196F3",
+    success: "#4CAF50",
+    warning: "#FB8C00",
+  },
+};
 
 // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
 export default createVuetify({
   theme: {
+    defaultTheme: "myDefaultTheme",
     themes: {
-      light: {
-        dark: true,
-        colors: {
-          primary: '#1867C0',
-          secondary: '#5CBBF6',
-        },
-      },
+      myDefaultTheme,
     },
   },
-})
+});
