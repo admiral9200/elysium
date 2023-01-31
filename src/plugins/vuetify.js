d582967d46cb306340580523efed4aab975c7464
Nicholas Wong <sword9200@gmail.com>
2023-01-31 14:27:12 +0800

    style: change color theme

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -12,14 +12,14 @@ import "vuetify/styles";
 import { createVuetify } from "vuetify";
 
 const myDefaultTheme = {
-  dark: true,
   colors: {
-    background: "#2B2B2B",
+    background: "#282828",
     surface: "#FFFFFF",
     primary: "#6200EE",
     "primary-darken-1": "#3700B3",
     secondary: "#03DAC6",
     "secondary-darken-1": "#018786",
+    accent: "#2979FF",
     error: "#B00020",
     info: "#2196F3",
     success: "#4CAF50",
