1b2baf791406ead07d0a5c11d31915108d865139
Nicholas Wong <sword9200@gmail.com>
2023-02-01 11:17:15 +0800

    style: adjust logo font

--- src/layouts/default/AppBar.vue
+++ src/layouts/default/AppBar.vue
@@ -1,75 +1,95 @@
 <template>
-<v-app-bar flat color="black">
-    <v-app-bar-title>
-        <v-icon icon="mdi-circle-slice-4" />
-        Elysium
+  <v-app-bar color="black">
+    <v-app-bar-title class="font-weight-black text-h5">
+      <v-icon icon="mdi-circle-slice-4" />
+      Elysium
     </v-app-bar-title>
 
-    <v-text-field append-icon="mdi-magnify" label="Search" single-line hide-details></v-text-field>
+    <v-text-field
+      append-icon="mdi-magnify"
+      label="Search"
+      single-line
+      hide-details
+    ></v-text-field>
 
     <v-spacer></v-spacer>
 
-    <v-btn color="white" v-for="item in items" :key="item" :to="item.link" class="me-2" large :icon="item.icon">
+    <v-btn
+      color="white"
+      v-for="item in items"
+      :key="item"
+      :to="item.link"
+      class="me-2"
+      large
+      :icon="item.icon"
+    >
     </v-btn>
     <v-menu>
-        <template v-slot:activator="{ props }">
-            <v-btn icon="mdi-account-circle" v-bind="props"></v-btn>
-        </template>
+      <template v-slot:activator="{ props }">
+        <v-btn icon="mdi-account-circle" v-bind="props"></v-btn>
+      </template>
 
-        <v-list bg-color="background">
-            <v-list-item v-for="(item, i) in menu" :key="i" :prepend-icon="item.icon" :to="item.link" >
-                <v-list-item-title>{{ item.text }}</v-list-item-title>
-            </v-list-item>
-        </v-list>
+      <v-list bg-color="background">
+        <v-list-item
+          v-for="(item, i) in menu"
+          :key="i"
+          :prepend-icon="item.icon"
+          :to="item.link"
+        >
+          <v-list-item-title>{{ item.text }}</v-list-item-title>
+        </v-list-item>
+      </v-list>
     </v-menu>
-</v-app-bar>
+  </v-app-bar>
 </template>
 
 <script>
 export default {
-    name: "AppBar",
-    setup() {
-        const items = [{
-                text: "Discover",
-                icon: "mdi-creation",
-                link: "/",
-            },
-            {
-                text: "Cart",
-                icon: "mdi-cart",
-                link: "/cart",
-            },
-        ];
-        const menu = [{
-                text: "My Space",
-                icon: "mdi-space-invaders",
-                link: "/my-space",
-            },
-            {
-                text: "Manage Wallet",
-                icon: "mdi-wallet",
-                link: "/wallet",
-            },
-            {
-                text: "Setting",
-                icon: "mdi-cog",
-                link: "/setting",
-            },
-            {
-                text: "Help",
-                icon: "mdi-help",
-                link: "/help",
-            },
-            {
-                text: "Logout",
-                icon: "mdi-logout",
-                link: "/logout",
-            },
-        ];
-        return {
-            items,
-            menu,
-        };
-    },
+  name: "AppBar",
+  setup() {
+    const items = [
+      {
+        text: "Discover",
+        icon: "mdi-creation",
+        link: "/",
+      },
+      {
+        text: "Cart",
+        icon: "mdi-cart",
+        link: "/cart",
+      },
+    ];
+    const menu = [
+      {
+        text: "My Space",
+        icon: "mdi-space-invaders",
+        link: "/my-space",
+      },
+      {
+        text: "Manage Wallet",
+        icon: "mdi-wallet",
+        link: "/wallet",
+      },
+      {
+        text: "Setting",
+        icon: "mdi-cog",
+        link: "/setting",
+      },
+      {
+        text: "Help",
+        icon: "mdi-help",
+        link: "/help",
+      },
+      {
+        text: "Logout",
+        icon: "mdi-logout",
+        link: "/logout",
+      },
+    ];
+    return {
+      items,
+      menu,
+    };
+  },
 };
 </script>
