48cb33ea77f159307cf14b7def84a5bd6d8a124e
Nicholas Wong <sword9200@gmail.com>
2023-02-01 11:37:07 +0800

    build: myspace-activity

--- src/views/MySpace.vue
+++ src/views/MySpace.vue
@@ -19,7 +19,9 @@
         <v-window-item :value="2">
           <OnSale />
         </v-window-item>
-        <v-window-item :value="3"> </v-window-item>
+        <v-window-item :value="3">
+          <Activity />
+        </v-window-item>
       </v-window>
     </v-card>
   </v-container>
@@ -30,6 +32,7 @@ import { ref } from "vue";
 import Profile from "@/components/mySpace/profile.vue";
 import OwnedNFT from "@/components/mySpace/ownedNFT.vue";
 import OnSale from "@/components/mySpace/onSale.vue";
+import Activity from "@/components/mySpace/activity.vue";
 
 export default {
   name: "MySpace",
@@ -37,6 +40,7 @@ export default {
     Profile,
     OwnedNFT,
     OnSale,
+    Activity,
   },
   setup() {
     var tab = ref(1);
