0923247f1922a2d59476247db5686c9a8efaa368
Nicholas Wong <sword9200@gmail.com>
2023-06-16 18:41:48 +0800

    style: optimized ui/ux

--- src/components/collectionPage/viewNFT.vue
+++ src/components/collectionPage/viewNFT.vue
@@ -1,35 +1,73 @@
 <template>
-  <v-card theme="dark" width="350px" class="pa-4">
-    <v-img height="194" :src="nft.tokenUri"></v-img>
-    <v-card-title>{{ nft.name }}</v-card-title>
-    <v-card-subtitle>{{ nft.desc }}</v-card-subtitle>
-    <div v-if="nft.price">
-      <v-card-text>Seller:</v-card-text>
-      <v-card-subtitle> {{ nft.seller }}</v-card-subtitle>
+  <v-card theme="dark" width="350px" class="py-2 px-1">
+    <v-alert
+      v-if="alert.show"
+      class="my-3 mx-6"
+      theme="dark"
+      :color="alert.color"
+      :icon="alert.icon"
+      :title="alert.title"
+      :text="alert.text"
+      variant="tonal"
+      density="compact"
+    ></v-alert>
+    <div v-if="!isUpdate && !isLoading">
+      <v-img height="194" :src="nft.tokenUri"></v-img>
+      <v-card-title>{{ nft.name }}</v-card-title>
+      <v-card-subtitle>{{ nft.desc }}</v-card-subtitle>
+      <div v-if="nft.price">
+        <v-card-text>Seller:</v-card-text>
+        <v-card-subtitle> {{ nft.seller }}</v-card-subtitle>
+      </div>
+      <div v-else>
+        <v-card-text>Owner:</v-card-text>
+        <v-card-subtitle> {{ nft.owner }}</v-card-subtitle>
+      </div>
+      <div>
+        <v-card-text>Collection:</v-card-text>
+        <v-card-subtitle> {{ nft.collection }}</v-card-subtitle>
+      </div>
+      <v-card-text v-if="nft.price">{{ nft.price }} ETH</v-card-text>
+      <v-card-text>Royalty: {{ nft.royalty }} %</v-card-text>
+      <v-card-actions v-if="showForm">
+        <v-text-field
+          v-model="price"
+          label="Price"
+          variant="outlined"
+          density="compact"
+          suffix="ETH"
+          type="number"
+        ></v-text-field>
+      </v-card-actions>
     </div>
-    <div v-else>
-      <v-card-text>Owner:</v-card-text>
-      <v-card-subtitle> {{ nft.owner }}</v-card-subtitle>
-    </div>
-    <div>
-      <v-card-text>Collection:</v-card-text>
-      <v-card-subtitle> {{ nft.collection }}</v-card-subtitle>
-    </div>
-    <v-card-text v-if="nft.price">{{ nft.price }} ETH</v-card-text>
-    <v-card-text>Royalty: {{ nft.royalty }} %</v-card-text>
-    <v-card-actions v-if="showForm">
-      <v-text-field
-        v-model="price"
-        label="Price"
-        variant="outlined"
-        density="compact"
-        suffix="ETH"
-        type="number"
-      ></v-text-field>
-    </v-card-actions>
+    <v-responsive v-if="isLoading" class="mx-auto" height="200px">
+      <v-overlay
+        v-model="isLoading"
+        class="d-flex align-center justify-center"
+        contained
+        persistent
+      >
+        <v-progress-circular
+          color="primary"
+          indeterminate
+          size="60"
+          width="8"
+        ></v-progress-circular>
+        <h3 class="ms-3 d-inline-block">Loading transaction...</h3>
+      </v-overlay>
+    </v-responsive>
     <v-card-actions class="d-flex justify-end">
       <v-btn
-        v-if="!showForm"
+        v-if="isUpdate"
+        class="me-2"
+        color="primary"
+        variant="outlined"
+        @click="refresh()"
+      >
+        Refresh
+      </v-btn>
+      <v-btn
+        v-if="!showForm && !isUpdate && !isLoading"
         class="me-2"
         variant="tonal"
         @click="$emit('onClose')"
@@ -39,7 +77,7 @@
       <v-btn
         color="primary"
         variant="outlined"
-        v-if="isOwner && !nft.price && !showForm"
+        v-if="isOwner && !nft.price && !showForm && !isUpdate && !isLoading"
         @click="showForm = true"
       >
         Sell
@@ -47,7 +85,7 @@
       <v-btn
         color="red"
         variant="outlined"
-        v-if="isSeller && nft.price"
+        v-if="isSeller && nft.price && !isUpdate && !isLoading"
         @click="unList(nft.collection, nft.tokenId)"
       >
         Cancel Sell
@@ -55,7 +93,7 @@
       <v-btn
         color="primary"
         variant="outlined"
-        v-if="!isSeller && nft.price"
+        v-if="!isSeller && nft.price && !isUpdate && !isLoading"
         @click="buy(nft.collection, nft.tokenId, nft.price)"
       >
         Buy
@@ -63,14 +101,18 @@
       <v-btn
         color="primary"
         variant="outlined"
-        v-if="!isSeller && nft.price"
+        v-if="!isSeller && nft.price && !isUpdate && !isLoading"
         @click="addCart(nft.collection, nft.tokenId)"
       >
         Add Cart
       </v-btn>
-      <v-btn v-if="showForm" @click="showForm = false">Cancel</v-btn>
       <v-btn
-        v-if="showForm"
+        v-if="showForm && !isUpdate && !isLoading"
+        @click="showForm = false"
+        >Cancel</v-btn
+      >
+      <v-btn
+        v-if="showForm && !isUpdate && !isLoading"
         color="primary"
         variant="tonal"
         @click="sell(nft.collection, nft.tokenId)"
@@ -93,6 +135,15 @@ export default {
     const { linkCollection, listNFT, unListNFT, buyNFT } = useMarketStore();
     const price = ref();
     const showForm = ref(false);
+    const isLoading = ref(false);
+    const isUpdate = ref(false);
+    const alert = ref({
+      show: false,
+      color: "",
+      icon: "",
+      title: "",
+      text: "",
+    });
 
     const isOwner = computed(() => {
       const nftOwner = props.nft.owner;
@@ -108,30 +159,83 @@ export default {
       else return false;
     });
 
+    const setAlert = (status, msg) => {
+      if (status === "error") {
+        alert.value = {
+          show: true,
+          color: "error",
+          icon: "$error",
+          title: "Oops...",
+          text: msg,
+        };
+      } else if (status === "success") {
+        alert.value = {
+          show: true,
+          color: "success",
+          icon: "$success",
+          title: "Success",
+          text: msg,
+        };
+      }
+    };
+
     const sell = async (nftCollection, nftId) => {
       try {
+        isLoading.value = true;
         const res = await listNFT(nftCollection, nftId, price.value.toString());
         console.log(res);
+        setAlert(
+          "success",
+          "Successfully listed NFT for sale! Please refresh page to update"
+        );
+        isLoading.value = false;
+        isUpdate.value = true;
       } catch (err) {
+        setAlert(
+          "error",
+          "We are facing some issues please try again later..."
+        );
         console.log(err);
       }
     };
 
     const unList = async (nftCollection, nftId) => {
       try {
+        isLoading.value = true;
         const res = await unListNFT(nftCollection, nftId);
         console.log(res);
+        setAlert(
+          "success",
+          "Successfully remove NFT from listing! Please refresh page to update"
+        );
+        isLoading.value = false;
+        isUpdate.value = true;
       } catch (err) {
+        setAlert(
+          "error",
+          "We are facing some issues please try again later..."
+        );
         console.log(err);
       }
     };
 
     const buy = async (nftCollection, nftId, nftPrice) => {
       try {
+        isLoading.value = true;
         const res = await buyNFT(nftCollection, nftId, nftPrice);
         console.log(res);
         await linkCollection(sessionStorage.getItem("address"), nftCollection);
+        setAlert(
+          "success",
+          "Successfully purchased NFT! Please refresh page to update"
+        );
+        isLoading.value = false;
+        isUpdate.value = true;
       } catch (err) {
+        setAlert(
+          "error",
+          "We are facing some issues please try again later..."
+        );
         console.log(err);
       }
     };
@@ -146,11 +250,16 @@ export default {
         if (res.data === "Cart not found") {
           nfts.value.push({ collection: nftCollection, tokenId: nftId });
           try {
-            const res = await axios.post("/api/cart", {
+            await axios.post("/api/cart", {
               user_address: sessionStorage.getItem("address"),
               cart_content: nfts.value,
             });
+            setAlert("success", "Successfully added to cart!");
           } catch (err) {
+            setAlert(
+              "error",
+              "We are facing some issues please try again later..."
+            );
             console.log(err);
           }
         } else {
@@ -162,6 +271,7 @@ export default {
               element.tokenId === nftId
             ) {
               exist = true;
+              setAlert("error", "NFT already in cart... ");
               console.log("NFT already in cart");
               break;
             }
@@ -169,30 +279,45 @@ export default {
           if (!exist) {
             nfts.value.push({ collection: nftCollection, tokenId: nftId });
             try {
-              const res = await axios.put("/api/cart", {
+              await axios.put("/api/cart", {
                 user_address: sessionStorage.getItem("address"),
                 cart_content: nfts.value,
               });
-              console.log(res);
+              setAlert("success", "Successfully added to cart!");
             } catch (err) {
+              setAlert(
+                "error",
+                "We are facing some issues please try again later..."
+              );
               console.log(err);
             }
           }
         }
       } catch (err) {
+        setAlert(
+          "error",
+          "We are facing some issues please try again later..."
+        );
         console.log(err);
       }
     };
 
+    const refresh = () => {
+      window.location.reload();
+    };
     return {
       isOwner,
       isSeller,
+      isLoading,
+      isUpdate,
       showForm,
       price,
+      alert,
       sell,
       unList,
       buy,
       addCart,
+      refresh,
     };
   },
 };
