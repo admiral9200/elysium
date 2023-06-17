6580cba2e34e790aa95cc892a3b5f2124d7a8d97
Nicholas Wong <sword9200@gmail.com>
2023-06-18 07:17:34 +0800

    style: optimized ui/ux

--- src/components/home/TopCollection.vue
+++ src/components/home/TopCollection.vue
@@ -1,28 +1,71 @@
 <template>
-  <div class="font-weight-bold text-h4 mt-4">Top Collection</div>
-  <v-row class="mt-5" v-if="topCollections.length">
-    <v-col cols="12" md="4" v-for="item in topCollections" :key="item.address">
-      <v-card class="mx-auto" max-width="344" color="black">
-        <v-img :src="item.cover" height="200px"></v-img>
-        <v-card-title class="text-h5">{{ item.name }}</v-card-title>
-        <v-card-text>{{ item.address }}</v-card-text>
-        <v-card-actions class="d-flex justify-space-between mx-2">
-          <v-btn variant="outlined">{{ item.counts }} Followers</v-btn>
-          <v-btn class="w-50" variant="outlined" :to="item.link">View</v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-col>
-  </v-row>
+  <div v-if="isExist">
+    <div class="font-weight-bold text-h4 mt-4">Top Collection</div>
+    <v-row class="mt-5" v-if="isLoading || topCollections.length">
+      <v-col
+        cols="12"
+        md="4"
+        v-for="item in topCollections"
+        :key="item.address"
+      >
+        <v-card class="mx-auto" max-width="344" color="black">
+          <v-img :src="item.cover" height="200px"></v-img>
+          <v-card-title class="text-h5">{{ item.name }}</v-card-title>
+          <v-card-text>{{ item.address }}</v-card-text>
+          <v-card-actions class="d-flex justify-space-between mx-2">
+            <v-btn variant="outlined">{{ item.counts }} Followers</v-btn>
+            <v-btn class="w-50" variant="outlined" :to="item.link">View</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+      <v-col cols="12" md="4" v-if="isLoading">
+        <v-skeleton-loader
+          class="mx-auto"
+          :loading="isLoading"
+          type="card,  list-item-two-line, actions"
+          width="344"
+          height="350"
+          theme="dark"
+        ></v-skeleton-loader>
+      </v-col>
+      <v-col cols="12" md="4" v-if="isLoading">
+        <v-skeleton-loader
+          class="mx-auto"
+          :loading="isLoading"
+          type="card,  list-item-two-line, actions"
+          width="344"
+          height="350"
+          theme="dark"
+        ></v-skeleton-loader>
+      </v-col>
+      <v-col cols="12" md="4" v-if="isLoading">
+        <v-skeleton-loader
+          class="mx-auto"
+          :loading="isLoading"
+          type="card,  list-item-two-line, actions"
+          width="344"
+          height="350"
+          theme="dark"
+        ></v-skeleton-loader>
+      </v-col>
+    </v-row>
+  </div>
 </template>
 
 <script>
 import { ref, onMounted } from "vue";
 import axios from "axios";
 import { useMarketStore } from "@/stores/market";
+import { VSkeletonLoader } from "vuetify/labs/VSkeletonLoader";
 
 export default {
   name: "TopCollection",
+  components: {
+    VSkeletonLoader,
+  },
   setup() {
+    const isExist = ref(true);
+    const isLoading = ref(true);
     const { getCollectionDetails } = useMarketStore();
     const topCollections = ref([]);
 
@@ -39,14 +82,18 @@ export default {
             topCollections.value.push(collectionItem);
           }
         }
+        isLoading.value = false;
       } catch (err) {
         if (err.response.status === 404)
           // TODO if no top collection, show something else
-          console.log(err);
+          isExist.value = false;
+        console.log(err);
       }
     });
 
     return {
+      isExist,
+      isLoading,
       topCollections,
     };
   },
--- src/components/home/TopUser.vue
+++ src/components/home/TopUser.vue
@@ -1,27 +1,68 @@
 <template>
-  <div class="font-weight-bold text-h4 mt-4">Most Popular Users</div>
-  <v-row class="mt-5" v-if="topUsers.length">
-    <v-col cols="12" md="4" v-for="item in topUsers" :key="item.address">
-      <v-card class="mx-auto py-3" max-width="344" color="black">
-        <v-img :src="item.profile_url" height="200px"></v-img>
-        <v-card-title class="text-h5">{{ item.username }}</v-card-title>
-        <v-card-text>{{ item.address }}</v-card-text>
-        <v-card-actions class="d-flex justify-space-between mx-2">
-          <v-btn variant="outlined">{{ item.followers }} Followers</v-btn>
-          <v-btn class="w-50" variant="outlined" :to="item.link">View</v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-col>
-  </v-row>
+  <div v-if="isExist">
+    <div class="font-weight-bold text-h4 mt-4">Most Popular Users</div>
+    <v-row class="mt-5" v-if="isLoading || topUsers.length">
+      <v-col cols="12" md="4" v-for="item in topUsers" :key="item.address">
+        <v-card class="mx-auto py-3" max-width="344" color="black">
+          <v-img :src="item.profile_url" height="200px"></v-img>
+          <v-card-title class="text-h5">{{ item.username }}</v-card-title>
+          <v-card-text>{{ item.address }}</v-card-text>
+          <v-card-actions class="d-flex justify-space-between mx-2">
+            <v-btn variant="outlined">{{ item.followers }} Followers</v-btn>
+            <v-btn class="w-50" variant="outlined" :to="item.link">View</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+      <v-col cols="12" md="4" v-if="isLoading">
+        <v-skeleton-loader
+          class="mx-auto"
+          :loading="isLoading"
+          type="image, heading, list-item, actions"
+          width="344"
+          height="375"
+          theme="dark"
+        >
+        </v-skeleton-loader>
+      </v-col>
+      <v-col cols="12" md="4" v-if="isLoading">
+        <v-skeleton-loader
+          class="mx-auto"
+          :loading="isLoading"
+          type="image, heading, list-item, actions"
+          width="344"
+          height="375"
+          theme="dark"
+        >
+        </v-skeleton-loader>
+      </v-col>
+      <v-col cols="12" md="4" v-if="isLoading">
+        <v-skeleton-loader
+          class="mx-auto"
+          :loading="isLoading"
+          type="image, heading, list-item, actions"
+          width="344"
+          height="375"
+          theme="dark"
+        >
+        </v-skeleton-loader>
+      </v-col>
+    </v-row>
+  </div>
 </template>
 
 <script>
 import { ref, onMounted } from "vue";
 import axios from "axios";
+import { VSkeletonLoader } from "vuetify/labs/VSkeletonLoader";
 
 export default {
   name: "TopUser",
+  components: {
+    VSkeletonLoader,
+  },
   setup() {
+    const isExist = ref(true);
+    const isLoading = ref(true);
     const topUsers = ref([]);
 
     onMounted(async () => {
@@ -35,14 +76,18 @@ export default {
             topUsers.value.push(user.data);
             console.log("User", user.data);
           }
+        isLoading.value = false;
       } catch (err) {
         if (err.response.status === 404)
           //TODO if no top seller, show something else
-          console.log(err);
+          isExist.value = false;
+        console.log(err);
       }
     });
 
     return {
+      isExist,
+      isLoading,
       topUsers,
     };
   },
