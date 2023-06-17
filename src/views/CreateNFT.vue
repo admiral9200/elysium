05aabb67c4d916536824b4ca15bc93e06bbd04f1
Nicholas Wong <sword9200@gmail.com>
2023-06-17 21:13:49 +0800

    style: optimized ui/ux fix: validation bugs

--- src/views/CreateNFT.vue
+++ src/views/CreateNFT.vue
@@ -14,86 +14,95 @@
     <v-row>
       <v-col md="8" cols="12">
         <v-card variant="outlined" theme="dark">
-          <v-card-title class="text-white"> Create New NFT</v-card-title>
-          <v-form @submit.prevent>
-            <v-card-text>
-              <v-text-field
-                class="mb-2"
-                v-model="wallet"
-                label="Wallet Address"
-                variant="outlined"
-                density="compact"
-                readonly
-              ></v-text-field>
-              <v-select
-                class="my-4"
-                v-model="selectedCollection"
-                :rules="[rules.required]"
-                :items="collections"
-                label="Collection"
-                variant="outlined"
-                density="compact"
-                required
-              >
-              </v-select>
-              <v-file-input
-                class="my-4"
-                v-model="file"
-                :rules="[rules.required, rules.fileType]"
-                label="File"
-                show-size
-                variant="outlined"
-                density="compact"
-                required
-              ></v-file-input>
-              <v-text-field
-                class="my-4"
-                v-model="name"
-                :rules="[rules.required, rules.min, rules.maxName, rules.name]"
-                label="Name"
-                variant="outlined"
-                density="compact"
-                required
-              ></v-text-field>
-              <v-textarea
-                class="my-4"
-                v-model="description"
-                :rules="[rules.required, rules.min, rules.maxDescription]"
-                label="Description"
-                variant="outlined"
-                rows="3"
-                density="compact"
-                required
-              ></v-textarea>
-              <div class="d-flex justify-space-between align-center">
-                <div>
-                  <v-label>List it for Sale on Elysium?</v-label>
+          <div v-if="!isLoading">
+            <v-card-title class="text-white"> Create New NFT</v-card-title>
+            <v-form @submit.prevent>
+              <v-card-text>
+                <v-text-field
+                  class="mb-2"
+                  v-model="wallet"
+                  label="Wallet Address"
+                  variant="outlined"
+                  density="compact"
+                  readonly
+                ></v-text-field>
+                <v-select
+                  class="my-4"
+                  v-model="selectedCollection"
+                  :rules="[rules.required]"
+                  :items="collections"
+                  item-title="name"
+                  item-value="address"
+                  label="Collection"
+                  variant="outlined"
+                  density="compact"
+                  return-object
+                  required
+                >
+                </v-select>
+                <v-file-input
+                  class="my-4"
+                  v-model="file"
+                  :rules="[rules.required, rules.fileType]"
+                  label="File"
+                  show-size
+                  variant="outlined"
+                  density="compact"
+                  required
+                ></v-file-input>
+                <v-text-field
+                  class="my-4"
+                  v-model="name"
+                  :rules="[
+                    rules.required,
+                    rules.min,
+                    rules.maxName,
+                    rules.name,
+                  ]"
+                  label="Name"
+                  variant="outlined"
+                  density="compact"
+                  required
+                ></v-text-field>
+                <v-textarea
+                  class="my-4"
+                  v-model="description"
+                  :rules="[rules.required, rules.min, rules.maxDescription]"
+                  label="Description"
+                  variant="outlined"
+                  rows="3"
+                  density="compact"
+                  required
+                ></v-textarea>
+                <div class="d-flex justify-space-between align-center">
+                  <div>
+                    <v-label>List it for Sale on Elysium?</v-label>
+                  </div>
+                  <div>
+                    <v-switch
+                      v-model="onSale"
+                      true-value="Yes"
+                      false-value="No"
+                      color="white"
+                      density="compact"
+                    ></v-switch>
+                  </div>
                 </div>
-                <div>
-                  <v-switch
-                    v-model="onSale"
-                    true-value="Yes"
-                    false-value="No"
-                    color="white"
-                    density="compact"
-                  ></v-switch>
-                </div>
-              </div>
-              <v-text-field
-                class="my-4"
-                v-model="price"
-                :rules="[rules.required, rules.minPrice]"
-                label="Price"
-                variant="outlined"
-                density="compact"
-                suffix="ETH"
-                type="number"
-                min="0.001"
-                dirty
-                required
-                v-if="onSale === 'Yes'"
-              ></v-text-field>
-              <!-- <div class="d-flex justify-space-between align-center">
+                <v-text-field
+                  class="my-4"
+                  v-model="price"
+                  :rules="[rules.required, rules.minPrice]"
+                  label="Price"
+                  variant="outlined"
+                  density="compact"
+                  suffix="ETH"
+                  type="number"
+                  min="0.001"
+                  dirty
+                  required
+                  v-if="onSale === 'Yes'"
+                ></v-text-field>
+                <!-- <div class="d-flex justify-space-between align-center">
                 <div>
                   <v-label>Free Minting</v-label>
                 </div>
@@ -108,15 +117,41 @@
                   ></v-switch>
                 </div>
               </div> -->
-            </v-card-text>
-          </v-form>
+              </v-card-text>
+            </v-form>
+          </div>
+          <v-responsive
+            v-if="isLoading"
+            class="mx-auto text-center"
+            height="500px"
+          >
+            <v-overlay
+              v-model="isLoading"
+              class="align-center justify-center"
+              contained
+              persistent
+            >
+              <v-progress-circular
+                color="primary"
+                indeterminate
+                size="60"
+                width="8"
+              ></v-progress-circular>
+              <h4 class="mt-3">
+                {{ loadingMsg }}
+              </h4>
+            </v-overlay>
+          </v-responsive>
         </v-card>
       </v-col>
       <v-col md="4" cols="12">
         <v-card variant="outlined">
           <v-card-title class="text-white">Preview</v-card-title>
           <v-img :src="previewImg" contain max-height="200"></v-img>
-          <v-card-text> Collection: {{ collectionName }} </v-card-text>
+          <v-card-text> Collection: {{ selectedCollection.name }} </v-card-text>
+          <v-card-text>
+            Collection Address: {{ selectedCollection.address }}
+          </v-card-text>
           <v-card-text> Name: {{ name }} </v-card-text>
           <v-card-text> Description: {{ description }} </v-card-text>
           <v-card-text> For Sale: {{ onSale }} </v-card-text>
@@ -157,9 +192,13 @@ export default {
     } = useMarketStore();
 
     const wallet = sessionStorage.getItem("address");
-    const collections = ref("");
-    const collectionDetails = ref([]);
-    const selectedCollection = ref("");
+    const isLoading = ref(false);
+    const loadingMsg = ref("nothing");
+    const collections = ref([]);
+    const selectedCollection = ref({
+      name: "",
+      address: "",
+    });
     const name = ref("");
     const description = ref("");
     const onSale = ref("No");
@@ -198,8 +237,8 @@ export default {
           name.value.length <= 25 &&
           description.value.length >= 3 &&
           description.value.length <= 250 &&
-          rules.name(name.value) &&
-          rules.fileType(file.value) &&
+          rules.name(name.value) === true &&
+          rules.fileType(file.value) === true &&
           price.value >= 0.001
         );
       } else {
@@ -208,8 +247,8 @@ export default {
           name.value.length <= 25 &&
           description.value.length >= 3 &&
           description.value.length <= 250 &&
-          rules.name(name.value) &&
-          rules.fileType(file.value)
+          rules.name(name.value) === true &&
+          rules.fileType(file.value) === true
         );
       }
     });
@@ -221,19 +260,11 @@ export default {
       return "";
     });
 
-    const collectionName = computed(() => {
-      if (selectedCollection.value) {
-        for (const item of collectionDetails.value) {
-          if (item.address === selectedCollection.value) {
-            return item.name;
-          }
-        }
-      } else return "";
-    });
-
     const submit = async () => {
       if (valid.value === true) {
         console.log("submit");
+        isLoading.value = true;
+        loadingMsg.value = "Uploading the file to IPFS...";
         const fileData = await uploadFileToIPFS(file.value[0]);
         console.log("fileData.IpfsHash", fileData.IpfsHash);
         const json = {
@@ -243,14 +274,15 @@ export default {
         };
         const jsonFile = await uploadJSONToIPFS(json);
         console.log("file", jsonFile.IpfsHash);
+        loadingMsg.value = "Minting the NFT...";
         const tokenId = await mintNFT(
           sessionStorage.getItem("address"),
-          selectedCollection.value,
+          selectedCollection.value.address,
           jsonFile.IpfsHash
         );
         // const tokenId = await mintNFT(
         //   sessionStorage.getItem("address"),
-        //   selectedCollection.value,
+        //   selectedCollection.value.address,
         //   "QmRaWcj4SsKuYyaemp7upnjHxk44AtC13JBvzwGH3YbJzc"
         // );
 
@@ -258,13 +290,21 @@ export default {
 
         if (onSale.value === "Yes") {
           try {
+            loadingMsg.value = "Listing the NFT on sale...";
             const res = await listNFT(
-              selectedCollection.value,
+              selectedCollection.value.address,
               tokenId,
               price.value.toString()
             );
             console.log("listed on sale", res);
           } catch (err) {
+            alert.value = {
+              show: true,
+              color: "error",
+              icon: "$error",
+              title: "Oops...",
+              text: "We are facing some issues please try again later...",
+            };
             console.log(err);
           }
         }
@@ -275,7 +315,8 @@ export default {
           title: "Success",
           text: "NFT Minted Successfully",
         };
-        // reset();
+        isLoading.value = false;
+        reset();
       } else {
         alert.value = {
           show: true,
@@ -298,22 +339,54 @@ export default {
     };
 
     onMounted(async () => {
-      //TODO: redirect to create collection if no collection
-      const res = await getMyCollection();
-      collections.value = res;
-      selectedCollection.value = res[0];
-      for (let i = 0; i < res.length; i++) {
-        let collectionItem = await getCollectionDetails(res[i]);
-        collectionDetails.value.push(collectionItem);
+      try {
+        isLoading.value = true;
+        loadingMsg.value = "Fetching your collections...";
+        const res = await getMyCollection();
+        if (res.length === 0) {
+          alert.value = {
+            show: true,
+            color: "error",
+            icon: "$error",
+            title: "Oops...",
+            text: "You don't have any collection. Please create one first.",
+          };
+          loadingMsg.value = "Redirecting to create collection page...";
+          setTimeout(() => {
+            window.location.href = "/user/collection/";
+          }, 3000);
+          return;
+        } else {
+          for (let i = 0; i < res.length; i++) {
+            let collectionItem = await getCollectionDetails(res[i]);
+            let collection = {
+              address: res[i],
+              name: collectionItem.name,
+            };
+            collections.value.push(collection);
+          }
+          selectedCollection.value.address = collections.value[0].address;
+          selectedCollection.value.name = collections.value[0].name;
+          isLoading.value = false;
+        }
+      } catch (err) {
+        alert.value = {
+          show: true,
+          color: "error",
+          icon: "$error",
+          title: "Oops...",
+          text: "We are facing some issues please try again later...",
+        };
+        console.log(err);
       }
-      console.log("collectionDetails", collectionDetails.value);
     });
 
     return {
       wallet,
-      collections,
+      isLoading,
+      loadingMsg,
       selectedCollection,
-      collectionName,
+      collections,
       name,
       description,
       onSale,
